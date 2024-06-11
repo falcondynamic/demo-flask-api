e35510dcfcf5cec531d631b1cf90b64be43809ae
Mustafa Akbas <gyrfalcondynamics@gmail.com>
2024-06-11 13:42:31 +0100

    init flask

--- flask-api/app.py
+++ flask-api/app.py
@@ -3,9 +3,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
+@app.route('/api')
 def api():
-    data = request.get_json()
+    data = {
+        'message': 'Hello World!',
+    }
     return jsonify(data)
 
 if __name__ == '__main__':
